7d5a3ac5f7f8b6249509daaa53931934936ca3c7
Srujan Joshi <44235961+srujanjoshi@users.noreply.github.com>
2021-01-26 16:31:41 +0530

    Expanded the Keys of the Xylophone to fit the whole screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int clipNumber) {
+    final player = AudioCache();
+    player.play("note$clipNumber.wav");
+  }
+
+  Expanded buildKey({Color color, int clipNumber}) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.zero,
+        color: color,
+        onPressed: () {
+          playSound(clipNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, clipNumber: 1),
+              buildKey(color: Colors.orange, clipNumber: 2),
+              buildKey(color: Colors.yellow, clipNumber: 3),
+              buildKey(color: Colors.green, clipNumber: 4),
+              buildKey(color: Colors.teal, clipNumber: 5),
+              buildKey(color: Colors.blue, clipNumber: 6),
+              buildKey(color: Colors.purple, clipNumber: 7),
+            ],
+          ),
         ),
       ),
     );
